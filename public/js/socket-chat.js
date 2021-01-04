96ad51259b2218a292a1eab8a7aac1a0ff5fade7
Gabriel <aballaygabriel@gmail.com>
2021-01-03 23:17:38 -0300

    chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,24 +1,27 @@
 var socket = io();
 
-var params= new URLSearchParams(window.location.search);
+var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre')|| !params.has('sala')){
-    window.location='index.html';
-    throw new Error('El nombre y sala son necesario');
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
-var usuario={
+var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    
-    socket.emit('entrarChat', usuario, function(resp){
-        console.log('usuario conectados ',resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuario(resp);
     });
+
 });
 
 // escuchar
@@ -30,25 +33,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
     console.log('Servidor:', mensaje);
+    renderizarMensaje(mensaje, false);
+    scrollBottom();
 });
 
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-socket.on('listaPersonas', function(data){
-    console.log(data)
+socket.on('listaPersonas', function(personas) {
+    renderizarUsuario(personas);
 });
 
 // Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-socket.on('mensajePrivado', function(mensaje){
-    console.log('Mensaje Privado: ',mensaje);
+    console.log('Mensaje Privado:', mensaje);
+
 });
